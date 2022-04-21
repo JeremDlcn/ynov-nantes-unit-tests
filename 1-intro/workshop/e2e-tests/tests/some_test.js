603cadd3182692e91e40e0db73ed1ac058cd005f
Paulin85 <46808735+Paulin85@users.noreply.github.com>
2022-04-21 15:57:52 +0200

    adding e2e test

--- 1-intro/workshop/e2e-tests/tests/some_test.js
+++ 1-intro/workshop/e2e-tests/tests/some_test.js
@@ -1,5 +1,10 @@
 Feature('Ynov Nantes');
 
-Scenario('Test Ynov Nantes Land Page', ({ I }) => {
+Scenario('Test Ynov Nantes Land Page', async ({ I }) => {
     I.amOnPage('https://www.ynov-nantes.com/');
+    I.click('.cta');
+    I.waitForElement('.searchfield');
+    I.fillField('.searchfield', 'info');
+    const value = await I.grabTextFrom('.preview-title .tile-title');
+    I.see('Bachelor Informatique', '.preview-title .tile-title');
 });
