b57e4581632959a1b641eaf6e2d86015a1d2e588
Paulin85 <46808735+Paulin85@users.noreply.github.com>
2022-04-28 15:04:44 +0200

    feat: add 1 when nearby mine

--- 3-test-driven-development/workshop/minesweeper.js
+++ 3-test-driven-development/workshop/minesweeper.js
@@ -3,14 +3,17 @@ function createBoard(n, m, mine) {
     if (n * m <= 0) {
         return "Game Over";
     } else {
-
         for (let line = 0; line < n; line++) {
             for (let col = 0; col < m; col++) {
                 if (mine > 0) {
                     board += "*";
                     mine--;
                 } else {
-                    board += ".";
+                        if (board[line - 1] === "*" || board[col - 1] === "*") {
+                            board = board + "1";
+                        } else {
+                            board += "0";
+                        }
                 }
             }
             board += "\n";
