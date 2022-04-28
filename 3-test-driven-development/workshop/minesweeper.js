e7ac36594e943cc2eb80cc7c37468fc894efd8a6
Paulin85 <46808735+Paulin85@users.noreply.github.com>
2022-04-28 13:38:14 +0200

    feat: adding mines in board

--- 3-test-driven-development/workshop/minesweeper.js
+++ 3-test-driven-development/workshop/minesweeper.js
@@ -1,11 +1,17 @@
-function createBoard(n, m) {
+function createBoard(n, m, mine) {
     let board = "";
     if (n * m <= 0) {
         return "Game Over";
     } else {
+
         for (let line = 0; line < n; line++) {
             for (let col = 0; col < m; col++) {
-                board += ".";
+                if (mine > 0) {
+                    board += "*";
+                    mine--;
+                } else {
+                    board += ".";
+                }
             }
             board += "\n";
         }
