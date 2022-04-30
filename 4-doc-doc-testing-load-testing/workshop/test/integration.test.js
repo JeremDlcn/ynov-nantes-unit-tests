1a077a54ea7c042d85b9e402f90b1bedfc417e44
Jerem Dlcn <44464165+JeremDlcn@users.noreply.github.com>
2022-04-30 16:13:32 +0200

    test: adding integration tests

--- 4-doc-doc-testing-load-testing/workshop/test/integration.test.js
+++ 4-doc-doc-testing-load-testing/workshop/test/integration.test.js
@@ -1,6 +1,7 @@
 const { expect } = require('@jest/globals');
 const ToDo = require('../toDoModel');
 const mongoose = require('mongoose');
+const mockingoose = require('mockingoose');
 
 
 mongoose.connect('mongodb+srv://ynovtest:ynovtest@cluster0.jdgyg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
@@ -8,22 +9,35 @@ mongoose.connect('mongodb+srv://ynovtest:ynovtest@cluster0.jdgyg.mongodb.net/myF
 
 
 // Test création d'un item
-test('Should Create an item', async() => {
-    const todo = await ToDo.create({
-            text: 'Test Integration',
-            done: false
-        })
-        // Trouver l'item
-    jest.spyOn(ToDo, 'findOne').mockReturnValue(Promise.resolve({ text: "Test Integration", done: false }))
+test('Should find by id', () => {
+    const todo = {
+        done: false,
+        _id: "626d367b866bfe2cd853c87b",
+        text: 'Test Integration',
+        __v: 0
+      }
+
+    mockingoose(ToDo).toReturn(todo, 'findOne');
+
+    return ToDo.findById(todo._id).then(result => {
+        expect(result.text).toBe(todo.text);
+        expect(result.done).toBe(todo.done);
+    })
 })
 
 
 // Test de la mise à jour d'un item
-test('Should Update an item', async() => {
-    const item = await ToDo.create({
-        text: 'Test Integration',
-        done: false
-    })
+test('Should create an item', async() => {
+    const todo = {
+        done: false,
+        text: 'Nouvelle Tâche',
+      }
 
-    jest.spyOn(ToDo, 'findOneAndUpdate').mockReturnValue(Promise.resolve({ text: "Test Integratio", done: false }))
+    mockingoose(ToDo).toReturn(todo, 'save');
+
+
+    return ToDo.create(todo).then(result => {
+        expect(result.text).toBe(todo.text);
+        expect(result.done).toBe(todo.done);
+    })
 })
